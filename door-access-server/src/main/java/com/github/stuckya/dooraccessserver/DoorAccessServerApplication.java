796cf0e6aae273dbee8f952559c9a3eb504d3fd9
andrewstucky <andrew.stucky@greenlight.guru>
2021-07-23 16:36:26 -0400

    Adds starters for server and web-client.

---
+++ door-access-server/src/main/java/com/github/stuckya/dooraccessserver/DoorAccessServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.stuckya.dooraccessserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorAccessServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorAccessServerApplication.class, args);
+	}
+
+}
---
+++ door-access-server/src/test/java/com/github/stuckya/dooraccessserver/DoorAccessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stuckya.dooraccessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorAccessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
