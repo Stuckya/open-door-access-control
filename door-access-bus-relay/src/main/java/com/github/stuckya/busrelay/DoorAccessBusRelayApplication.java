24264bb691971dd2880e053ae2d8ccb7b733cf2b
andrewstucky <andrew.stucky@greenlight.guru>
2021-07-30 17:00:51 -0400

    Bus relay package naming update.

--- door-access-bus-relay/src/main/java/com/github/stuckya/busrelay/DoorAccessBusRelayApplication.java
+++ door-access-bus-relay/src/main/java/com/github/stuckya/busrelay/DoorAccessBusRelayApplication.java
@@ -1,4 +1,4 @@
-package com.github.stuckya.dooraccessbusrelay;
+package com.github.stuckya.busrelay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- door-access-bus-relay/src/test/java/com/github/stuckya/busrelay/DoorAccessBusRelayApplicationTests.java
+++ door-access-bus-relay/src/test/java/com/github/stuckya/busrelay/DoorAccessBusRelayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.stuckya.dooraccessbusrelay;
+package com.github.stuckya.busrelay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
